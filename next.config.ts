d9d2d52b2d1f8cafa1dd1e5543b169c9427df013
jav974 <jeremyvienne@gmail.com>
2025-06-15 22:06:28 +0200

    include actions and dataflow dirs in eslint config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  eslint: {
+    dirs: ['app', 'actions', 'dataflow'],
+  },
 };
 
 export default nextConfig;
