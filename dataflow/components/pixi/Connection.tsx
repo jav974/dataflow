2e153e8c021ac4d06ad908f6cf8393df421fb051
jav974 <jeremyvienne@gmail.com>
2025-06-16 20:47:44 +0200

    fixed Connection positioning

--- dataflow/components/pixi/Connection.tsx
+++ dataflow/components/pixi/Connection.tsx
@@ -5,7 +5,7 @@ import { Node, Pin, useNodeContext } from "@/dataflow/contexts/NodeContext";
 import { LineTextures } from "./textures";
 import BezierCurve from "./BezierCurve";
 import { useNodeLastUpdated } from "@/dataflow/hooks/useLastUpdated";
-import { batch, RefSignal, useRefSignal, useRefSignalEffect, useRefSignalRender } from "react-refsignal";
+import { useRefSignal, useRefSignalEffect, useRefSignalRender } from "react-refsignal";
 
 interface ConnectionProps {
     from: ConnectorConfig;
@@ -14,10 +14,8 @@ interface ConnectionProps {
 
 export default function Connection({from, to}: ConnectionProps) {
     const { nodes } = useNodeContext();
-    const fromNode = useRef<RefSignal<Node> | undefined>(undefined);
-    const toNode = useRef<RefSignal<Node> | undefined>(undefined);
-    const trackedFrom = useRefSignal<Node | undefined>(undefined);
-    const trackedTo = useRefSignal<Node | undefined>(undefined);
+    const fromNode = useRefSignal<Node | undefined>(undefined);
+    const toNode = useRefSignal<Node | undefined>(undefined);
     const fromPin = useRef<Pin | undefined>(undefined);
     const toPin = useRef<Pin | undefined>(undefined);
     const fromPos = useRef<Coordinates | undefined>(undefined);
@@ -26,10 +24,21 @@ export default function Connection({from, to}: ConnectionProps) {
     const fromNodeUpdatedAt = useNodeLastUpdated(from.id);
     const toNodeUpdatedAt = useNodeLastUpdated(to.id);
 
+    // Re-render component when fromNode and toNode have been set/updated
+    useRefSignalRender([fromNode, toNode], () =>
+        fromNode.current !== undefined && toNode.current !== undefined
+    );
+
     const computePositions = useCallback(() => {
         if (fromNode.current && fromPin.current && toNode.current && toPin.current) {
-            const tmpFrom: Coordinates = { x: fromNode.current.current.mutableNodeConfig.position.x + fromPin.current.position.x, y: fromNode.current.current.mutableNodeConfig.position.y + fromPin.current.position.y };
-            const tmpTo: Coordinates = { x: toNode.current.current.mutableNodeConfig.position.x + toPin.current.position.x, y: toNode.current.current.mutableNodeConfig.position.y + toPin.current.position.y };
+            const tmpFrom: Coordinates = {
+                x: fromNode.current.mutableNodeConfig.position.x + fromPin.current.position.x,
+                y: fromNode.current.mutableNodeConfig.position.y + fromPin.current.position.y
+            };
+            const tmpTo: Coordinates = {
+                x: toNode.current.mutableNodeConfig.position.x + toPin.current.position.x,
+                y: toNode.current.mutableNodeConfig.position.y + toPin.current.position.y
+            };
 
             if (fromPos.current?.x !== tmpFrom.x || fromPos.current?.y !== tmpFrom.y
                 || toPos.current?.x !== tmpTo.x || toPos.current?.y !== tmpTo.y
@@ -41,32 +50,47 @@ export default function Connection({from, to}: ConnectionProps) {
             fromPos.current = undefined;
             toPos.current = undefined;
         }
-    }, []);
+    }, [fromNode, toNode]);
 
-    // Re-render component when fromNode or toNode has been set
-    useRefSignalRender([trackedFrom, trackedTo]);
+    const updateTrackedFrom = useCallback((value: Node) => {
+        fromNode.update(value);
+    }, [fromNode]);
 
-    // Initialize fromNode and toNode after nodes update
+    const updateTrackedTo = useCallback((value: Node) => {
+        toNode.update(value);
+    }, [toNode]);
+
+    // Initialize fromNode and toNode on initial mount and after nodes update
     useRefSignalEffect(() => {
-        fromNode.current = nodes.current.get(from.id);
-        toNode.current = nodes.current.get(to.id);
+        const _fromNode = nodes.current.get(from.id);
+        const _toNode = nodes.current.get(to.id);
 
-        batch(() => {
-            trackedFrom.update(fromNode.current?.current);
-            trackedTo.update(toNode.current?.current);
-        }, [trackedFrom, trackedTo]);
-    }, [nodes]);
+        if (_fromNode && _fromNode.current.mutableNodeConfig.id !== fromNode.current?.mutableNodeConfig.id) {
+            _fromNode.subscribe(updateTrackedFrom);
+            fromNode.update(_fromNode.current);
+        }
+
+        if (_toNode && _toNode.current.mutableNodeConfig.id !== toNode.current?.mutableNodeConfig.id) {
+            _toNode.subscribe(updateTrackedTo);
+            toNode.update(_toNode.current);
+        }
+
+        return () => {
+            _fromNode?.unsubscribe(updateTrackedFrom);
+            _toNode?.unsubscribe(updateTrackedTo);
+        };
+    }, [nodes, updateTrackedFrom, updateTrackedTo]);
 
     // Initialize pins after fromNode and toNode update
     useRefSignalEffect(() => {
         if (fromNode.current && toNode.current) {
             // Output (value) to Input
             if (from.pin !== 'continue' && to.pin !== 'execute') {
-                fromPin.current = fromNode.current.current.outputs.find(output => output.id === from.pin);
-                toPin.current = toNode.current.current.inputs.find(input => input.id === to.pin);
+                fromPin.current = fromNode.current.outputs.find(output => output.id === from.pin);
+                toPin.current = toNode.current.inputs.find(input => input.id === to.pin);
 
-                const outputConfig = fromNode.current.current.mutableNodeConfig.outputs?.find(output => output.id === from.pin);
-                const inputConfig = toNode.current.current.mutableNodeConfig.inputs?.find(input => input.id === to.pin);
+                const outputConfig = fromNode.current.mutableNodeConfig.outputs?.find(output => output.id === from.pin);
+                const inputConfig = toNode.current.mutableNodeConfig.inputs?.find(input => input.id === to.pin);
 
                 if (outputConfig && inputConfig) {
                     // undefined should evaluate as false here, so keep this coercion operator !!
@@ -85,21 +109,21 @@ export default function Connection({from, to}: ConnectionProps) {
             }
             // Output (branch) to Execute
             else if (from.pin !== 'continue' && to.pin === 'execute') {
-                fromPin.current = fromNode.current.current.branches.find(branch => branch.id === from.pin);
-                toPin.current = toNode.current.current.executePin;
+                fromPin.current = fromNode.current.branches.find(branch => branch.id === from.pin);
+                toPin.current = toNode.current.executePin;
                 setTexture(LineTextures.flow);
             }
             // Continue to Execute
             else if (from.pin === 'continue' && to.pin === 'execute') {
-                fromPin.current = fromNode.current.current.continuePin;
-                toPin.current = toNode.current.current.executePin;
+                fromPin.current = fromNode.current.continuePin;
+                toPin.current = toNode.current.executePin;
                 setTexture(LineTextures.flow);
             }
         }
 
         // Initial position compute
         computePositions();
-    }, [fromNode.current, toNode.current, computePositions]);
+    }, [fromNode, toNode, computePositions]);
 
     // Recompute position after node movement
     useEffect(() => {
