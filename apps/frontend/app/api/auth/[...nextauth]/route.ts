29caf0e368d5c0347ea6a63bd21a42a0dbc18b72
jav974 <jeremyvienne@gmail.com>
2025-07-07 12:20:25 +0200

    retrieve provider and id from oauth provider

--- apps/frontend/app/api/auth/[...nextauth]/route.ts
+++ apps/frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import GitHubProvider, { GithubProfile } from "next-auth/providers/github";
 
 const handler = NextAuth({
     providers: [
@@ -20,6 +20,24 @@ const handler = NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET!,
         }),
     ],
+    callbacks: {
+        async jwt({ token, account, profile }) {
+            if (profile) {
+                if (account?.provider === "google") {
+                    token.id = (profile as GoogleProfile).sub;
+                } else if (account?.provider === "github") {
+                    token.id = (profile as GithubProfile).id;
+                }
+                token.provider = account?.provider;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.user.id = token.id;
+            session.provider = token.provider as string;
+            return session;
+        }
+    }
 });
 
 export { handler as GET, handler as POST }
